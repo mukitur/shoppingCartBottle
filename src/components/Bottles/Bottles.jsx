3eec0e93dfda0f1bf07ad18798c7d17d5e4a31e8
mukitur <mukit.inway@gmail.com>
2023-09-12 00:01:04 +0600

    live link added

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import Bottle from '../Bottle/Bottle';
 import './Bottles.css';
+import { addToLocalStorage, getStoredCart } from '../../Utilities/localStorage';
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
@@ -12,10 +13,32 @@ const Bottles = () => {
       .then((data) => setBottles(data));
   }, []);
 
+  //   load cart from local storage
+  useEffect(() => {
+    console.log('called the useEffect', bottles.length);
+    if (bottles.length > 0) {
+      const storedCart = getStoredCart();
+      console.log(storedCart, bottles);
+      const savedCart = [];
+      for (const id of storedCart) {
+        console.log(id);
+        const bottle = bottles.find((bottle) => bottle.id === id);
+        if (bottle) {
+          savedCart.push(bottle);
+        }
+      }
+
+      console.log('saved cart', savedCart);
+      setCart(savedCart);
+    }
+  }, [bottles]);
+
   const handleAddToCart = (bottle) => {
     // console.log(bottle);
     const newCart = [...cart, bottle];
     setCart(newCart);
+    // save to localStorage
+    addToLocalStorage(bottle.id);
   };
   return (
     <>
